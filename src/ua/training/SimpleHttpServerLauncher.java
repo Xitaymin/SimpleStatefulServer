76d5180e2035b4a36b957d2e95bbda30bf888d6f
Xitaymin <y.barashenko@gmail.com>
2021-09-03 23:04:31 +0300

    hostname and port added in properties

--- src/ua/training/SimpleHttpServerLauncher.java
+++ src/ua/training/SimpleHttpServerLauncher.java
@@ -4,15 +4,27 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import ua.training.handler.MainHandler;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Properties;
 
 public class SimpleHttpServerLauncher implements GlobalConstants {
     private HttpHandler handler = new MainHandler();
     private HttpServer server;
 
     public void initial() throws IOException{
-        server = HttpServer.create(new InetSocketAddress("localhost",8080), 0);
+        //todo deal with possible npe
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "network.properties";
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(appConfigPath));
+
+        String hostname = properties.getProperty("host.name");
+        String port = properties.getProperty("port.number");
+
+        server = HttpServer.create(new InetSocketAddress(hostname, Integer.parseInt(port)), 0);
         server.createContext(APPLICATION_CONTEXT, handler);
         server.start();
     }
